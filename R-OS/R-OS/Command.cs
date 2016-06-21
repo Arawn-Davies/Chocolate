e3b4fab9779357cdcfc6071c546891b96d30ef45
Arawn Davies <bldsnw@gmail.com>
2016-06-21 22:18:50 +0100

    Whole lot done, added the new cmd line, and converted the standard if (input == "") to SendCommand(input) where SendCommand has (string input)
    alls looking very good

---
+++ R-OS/R-OS/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Sys = Cosmos.System;
+using ROS;
+using R_OS;
+
+namespace R_OS
+{
+    class Command
+    {
+        
+    }
+}
--- R-OS/R-OS/Kernel.cs
+++ R-OS/R-OS/Kernel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using Sys = Cosmos.System;
+using ROS;
+using R_OS;
 
 namespace ROS
 {
@@ -25,97 +27,15 @@ namespace ROS
         protected override void Run()
         {
             Console.Write(current_directory + " >");
-            var input = Console.ReadLine();
-            if (input == "help")
-            {
-                Console.WriteLine("Help:");
-                Console.WriteLine("'echo' args  --> Prints the text in place of 'args' onto the console.");
-                Console.WriteLine("'help'       --> Displays this help table.");
-                Console.WriteLine("'reboot'     --> Reboots R-OS.");
-                Console.WriteLine("'dir'        --> Prints a list of folders in the current directory");
-                Console.WriteLine("'mkdir' args --> Creates a new directory in the current folder with the name 'args'");
-                Console.WriteLine("'ls'         --> Prints a list of files in the current directory");
-
-            }
-            else if (input == "dir")
-            {
-                string[] folders = Directory.GetDirectories(current_directory);
-
-                foreach (var folder in folders)
-                {
-                    Console.WriteLine(folder);
-                }
-            }
-            else if (input == "ls")
-            {
-                string[] files = Directory.GetFiles(current_directory);
-
-                foreach (var file in files)
-                {
-                    Console.WriteLine(file);
-                }
-            }
-            else if (seperate(input, 2) == "cd")
-            {
-                if (input == "cd")
-                {
-                    Console.WriteLine(current_directory);
-                }
-                else
-                {
-                    var arg = rseperate(input, 3, (input.Length - 1));
-                    if (System.IO.Directory.Exists(current_directory + arg) == true)
-                    {
-                        System.IO.Directory.SetCurrentDirectory(current_directory + arg);
-                        current_directory = "0:\\" + System.IO.Directory.GetCurrentDirectory();
-                    }
-                    else if (System.IO.Directory.Exists(current_directory + arg) == false)
-                    {
-                        Console.WriteLine("Directory: " + arg + " : does not exist");
-                    }
-                }
-            }
-            else if (seperate(input, 5) == "mkdir")
-            {
-                if (input == "mkdir")
-                {
-                    Console.Write("mkdir takes 1 argument");
-                }
-                else
-                {
-                    var arg = rseperate(input, 6, (input.Length - 1));
-                    System.IO.Directory.CreateDirectory(current_directory + arg);
-                }
-            }
-            else if (input == "reboot")
-            {
-                Sys.Power.Reboot();
-            }
-            else if (seperate(input, 4) == "echo")
-            {
-                if (input == "echo")
-                {
-                    Console.Write("Echo takes 1 argument");
-                }
-                else
-                {
-                    var arg = rseperate(input, 5, (input.Length - 1));
-                    echo(arg);
-                }
-            }
-            else if (input == "")
-            {
-
-            }
-            else
-            {
-                Console.WriteLine("Error: " + input + " is not a recognised command, please see the help command.");
-            }
+            string input = Console.ReadLine();
+            SendCommand(input);
         }
+        /*
         public static void echo(string arg)
         {
             Console.Write("\n" +arg + "\n");
         }
+        
         public static string seperate(string input, int length)
         {
             var count = 0;
@@ -142,6 +62,89 @@ namespace ROS
                 count = count + 1;
             }
             return output;
+        }
+        */
+
+
+        public void SendCommand(string input)
+        {
+            string[] args = input.Split(' ');
+            if (input.StartsWith("reboot"))
+            {
+                Sys.Power.Reboot();
+            }
+            if (input.StartsWith("help"))
+            {
+                Console.WriteLine("Help:");
+                Console.WriteLine("'echo' args  --> Prints the text in place of 'args' onto the console.");
+                Console.WriteLine("'help'       --> Displays this help table.");
+                Console.WriteLine("'reboot'     --> Reboots R-OS.");
+                Console.WriteLine("'dir'        --> Prints a list of folders in the current directory");
+                Console.WriteLine("'mkdir' args --> Creates a new directory in the current folder with the name 'args'");
+                Console.WriteLine("'ls'         --> Prints a list of files in the current directory");
+
+            }
+            if (input.StartsWith("cd"))
+            {
+                if (input == "cd")
+                {
+                    Console.WriteLine(current_directory);
+                }
+                else
+                {
+                    var arg = input.Remove(0, 3);
+                    if (System.IO.Directory.Exists(current_directory + arg) == true)
+                    {
+                        System.IO.Directory.SetCurrentDirectory(current_directory + arg);
+                        current_directory = System.IO.Directory.GetCurrentDirectory();
+                    }
+                    else if (System.IO.Directory.Exists(current_directory + arg) == false)
+                    {
+                        Console.WriteLine("Directory: " + arg + " : does not exist");
+                    }
+                }
+            }
+            else if (input.StartsWith("dir"))
+            {
+                string[] folders = Directory.GetDirectories(current_directory);
+
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+            }
+            else if (input.StartsWith("mkdir "))
+            {
+                string dirname = input.Remove(0, 6);
+                System.IO.Directory.CreateDirectory(current_directory + dirname);
+            }
+            else if (input.StartsWith("ls"))
+            {
+                string[] files = Directory.GetFiles(current_directory);
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            else if (input.StartsWith("echo"))
+            {
+                try
+                {
+                    Console.WriteLine(input.Remove(0, 5));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("echo: " + ex.Message);
+                }
+            }
+            else if (input == "")
+            {
+            }
+            else
+            {
+                Console.WriteLine("Error: " + input + " is not a recognised command, please see the help command.");
+            }
         }
 
     }

919c7828d8f3bb474d4746b5ae3ab0846029870e
Arawn Davies <bldsnw@gmail.com>
2016-06-21 16:28:50 +0100

    Implemented most of the filesystems basic commands (ls, dir, cd, mkdir), a few issues regarding the cd command that it doesn't seem to change the directory or list it, but apart from that, it works good ^_^

--- R-OS/R-OS/Kernel.cs
+++ R-OS/R-OS/Kernel.cs
@@ -16,6 +16,7 @@ namespace ROS
             var FS = new Sys.FileSystem.CosmosVFS();
             Sys.FileSystem.VFS.VFSManager.RegisterVFS(FS);
             FS.Initialize();
+            Console.WriteLine("Filesystems Initialized.");
             Console.WriteLine("Welcome to R-OS");
             Console.WriteLine("Use 'help' to see a list of commands.");
             Console.WriteLine("System dir separator: " + Sys.FileSystem.VFS.VFSManager.GetDirectorySeparatorChar());
@@ -23,19 +24,56 @@ namespace ROS
 
         protected override void Run()
         {
-            Console.Write(current_directory + "/>");
+            Console.Write(current_directory + " >");
             var input = Console.ReadLine();
-            Console.Write("\n" + input);
             if (input == "help")
             {
                 Console.WriteLine("Help:");
-                Console.WriteLine("'echo' args --> Prints the text in place of 'args' onto the console.");
-                Console.WriteLine("'help'      --> Displays this help table.");
-                Console.WriteLine("'reboot'    --> Reboots R-OS.");
+                Console.WriteLine("'echo' args  --> Prints the text in place of 'args' onto the console.");
+                Console.WriteLine("'help'       --> Displays this help table.");
+                Console.WriteLine("'reboot'     --> Reboots R-OS.");
+                Console.WriteLine("'dir'        --> Prints a list of folders in the current directory");
+                Console.WriteLine("'mkdir' args --> Creates a new directory in the current folder with the name 'args'");
+                Console.WriteLine("'ls'         --> Prints a list of files in the current directory");
+
             }
             else if (input == "dir")
             {
-                Console.WriteLine("dir : Coming soon to R-OS!");
+                string[] folders = Directory.GetDirectories(current_directory);
+
+                foreach (var folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+            }
+            else if (input == "ls")
+            {
+                string[] files = Directory.GetFiles(current_directory);
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            else if (seperate(input, 2) == "cd")
+            {
+                if (input == "cd")
+                {
+                    Console.WriteLine(current_directory);
+                }
+                else
+                {
+                    var arg = rseperate(input, 3, (input.Length - 1));
+                    if (System.IO.Directory.Exists(current_directory + arg) == true)
+                    {
+                        System.IO.Directory.SetCurrentDirectory(current_directory + arg);
+                        current_directory = "0:\\" + System.IO.Directory.GetCurrentDirectory();
+                    }
+                    else if (System.IO.Directory.Exists(current_directory + arg) == false)
+                    {
+                        Console.WriteLine("Directory: " + arg + " : does not exist");
+                    }
+                }
             }
             else if (seperate(input, 5) == "mkdir")
             {
@@ -46,7 +84,7 @@ namespace ROS
                 else
                 {
                     var arg = rseperate(input, 6, (input.Length - 1));
-                    System.IO.Directory.CreateDirectory(arg);
+                    System.IO.Directory.CreateDirectory(current_directory + arg);
                 }
             }
             else if (input == "reboot")
@@ -64,6 +102,14 @@ namespace ROS
                     var arg = rseperate(input, 5, (input.Length - 1));
                     echo(arg);
                 }
+            }
+            else if (input == "")
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Error: " + input + " is not a recognised command, please see the help command.");
             }
         }
         public static void echo(string arg)
